62978e5d3aa3dd76413ebdc532c53d0dc9998635
Ishant Yadav <149149772+ishantyadv@users.noreply.github.com>
2025-02-18 20:37:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin:'https://tokyo-create-blog.netlify.app/',
     credentials: true
 }))
 
